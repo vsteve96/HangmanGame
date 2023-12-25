af673e48f857ae0fafbe7d2b52d5865fa85cb72c
vsteve96 <>
2023-12-25 14:11:24 +0000

    Set up functions that displays the words to be guessed

--- run.py
+++ run.py
@@ -1,2 +1,20 @@
 import random
 
+def choose_word():
+    """
+    A set of words that are chosen randomly at the start of the game
+    """
+    words = ["python", "hangman", "developer", "programming", "coding", ]
+    return random.choice(words)
+
+def display_word(word, guessed_letters):
+    """
+    Generates a display string for the word, revealing guessed letters and hiding others.
+        word: The secret word to be guessed.
+        guessed_letters (list): List of letters that the player has guessed.
+    Returns a string: The current word
+    """
+    display = ""
+    for letter in word:
+        display += letter if letter in guessed_letters else "_"
+    return display
